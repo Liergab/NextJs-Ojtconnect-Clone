53ffc4c8d429b4d9556e78d17f8343a6cf37114a
Bryan Gabriel <bryangabrielberja25@gmail.com>
2024-05-01 19:55:01 +0800

    auth

--- app/(root)/(student)/student-dashboard/page.tsx
+++ app/(root)/(student)/student-dashboard/page.tsx
@@ -1,8 +1,19 @@
+import { auth, signOut } from '@/auth'
 import React from 'react'
 
-const StudentDashboard = () => {
+const StudentDashboard = async() => {
+  const session = await auth()
   return (
-    <div>StudentDashboard</div>
+    <div>
+      <h1>StudentDashboard</h1>
+      <h1>{JSON.stringify(session)}</h1>
+      <form action={ async() => {
+        "use server"
+        await signOut()
+      }}>
+        <button type='submit'>sign out</button>
+      </form>
+  </div>
   )
 }
 
