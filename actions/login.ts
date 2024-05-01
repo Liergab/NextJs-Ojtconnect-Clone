0eb572f41e5d441061fc7d611f15d166f4c40916
Bryan Gabriel <bryangabrielberja25@gmail.com>
2024-05-01 19:46:16 +0800

    Add-signIn-from-authjs

--- actions/login.ts
+++ actions/login.ts
@@ -1,14 +1,31 @@
 "use server"
 import { LoginFormSchema } from '@/Types/FormTypes'
+import { signIn } from '@/auth'
 import db from '@/lib/db'
+import { Default_LOGIN_REDIRECT } from '@/routes'
+import { AuthError } from 'next-auth'
 import * as z from 'zod'
 
 export const login = async(values: z.infer<typeof LoginFormSchema>) =>{
-    const validatedFileds = LoginFormSchema.safeParse(values)
-  
-    if(!validatedFileds.success){
-        return{error:'Invalid Credentials'}
-    }
 
-    return {success:'Email sent'}
+    const {email, password} = values
+    try {
+      await signIn('credentials',{
+            email, 
+            password,
+            redirectTo:Default_LOGIN_REDIRECT
+        })
+
+    } catch (error) {
+        if(error instanceof AuthError){
+            switch (error.type) {
+                case "CredentialsSignin":
+                    return {error:'Invalid Credentials'}
+                default:
+                    return {error:'something went wrong'};
+            }
+
+        } 
+        throw error;
+    }
 }
